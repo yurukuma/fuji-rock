81a737cebec58d42e47ddbb818a0aed0776e688c
yuru <yurulin056@gmail.com>
2021-03-08 13:19:23 +0800

    deploy setting

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -5,10 +5,10 @@ set -e
 
 # build
 yarn run build
-cd docs/.vuepress/dist
+cd docs/src/.vuepress/dist
 git init
 git add -A
 git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f git@github.com:yurukuma/fuji-rock.git master:gh-pages
+git push -f git@github.com:yurukuma/fuji-rock.git main:gh-pages
 cd ../../../
